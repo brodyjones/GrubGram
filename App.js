2a892544508793c60c17c546300b8e6ee5ed64ec
keithhoffy7 <117609283+keithhoffy7@users.noreply.github.com>
2022-11-28 15:34:34 -0800

    made very crude version of user posting feature

--- App.js
+++ App.js
@@ -1,8 +1,69 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import {db} from './firebase-config'
+import {collection, getDocs, addDoc, deleteDoc, doc} from "firebase/firestore"
 
 function App() {
+  const [newImage, setNewImage] = useState("");
+  const [newCaption, setNewCaption] = useState("");
+  const [newRecipe, setNewRecipe] = useState("");
+
+  const [userPosts, setUserPosts] = useState([]);
+  const userPostsRef = collection(db, "user posts");
+
+  const createUserPost = async () => {
+    await addDoc(userPostsRef, {username: "keithhoffy", imagelink: newImage, 
+      caption: newCaption, datecreated: "11/28/22", recipe: newRecipe})
+  }
+
+  const deleteUserPost = async (id) => {
+    const userPostDoc = doc(db, "user posts", id);
+    await deleteDoc(userPostDoc);
+  }
+
+  useEffect(() => {
+    const getUserPosts = async () => {
+      const data = await getDocs(userPostsRef);
+      setUserPosts(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    }
+
+    getUserPosts()
+  }, [])
+
   return (
-    <div className="App"> </div>
+    <div className="App"> 
+    <input 
+      placeholder='Image...' 
+      onChange={(event) => {
+        setNewImage(event.target.value);
+      }}
+    />
+    <input 
+      placeholder='Caption...' 
+      onChange={(event) => 
+        {setNewCaption(event.target.value)
+      }}
+    />
+    <input 
+      placeholder='Recipe...' 
+      onChange={(event) => {
+          setNewRecipe(event.target.value)
+        }}
+      />
+    <button onClick={createUserPost}>Post</button>
+      {userPosts.map((userPost) => {
+        return (
+          <div>
+            <h1>User: {userPost.username} </h1>
+            <h1>Image Link: {userPost.imagelink} </h1>
+            <h1>Caption: {userPost.caption} </h1>
+            <h1>Date Created: {userPost.datecreated} </h1>
+            <h1>Recipe Used: {userPost.recipe} </h1>
+            <button onClick={() => {deleteUserPost(userPost.id)}}>Delete Post</button>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
