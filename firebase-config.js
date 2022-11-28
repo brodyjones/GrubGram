77335f2589b86a447e1aa6ae32583d48dd15382d
seanhoffy <117390844+seanhoffy@users.noreply.github.com>
2022-11-28 00:43:25 -0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDgw2Pva8wCPCHHSDa6CUqmCjUsZ3JkzOk",
+    authDomain: "grubgram-2bccc.firebaseapp.com",
+    projectId: "grubgram-2bccc",
+    storageBucket: "grubgram-2bccc.appspot.com",
+    messagingSenderId: "12941980945",
+    appId: "1:12941980945:web:6be4c61e23fe9dd57948b0",
+    measurementId: "G-TJY8Z1B1VJ"
+  };
