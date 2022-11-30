5745f1a8de10ac4866d49d158bc83a88f9b34d32
Sean Hoffmeister <seanhoffy@g.ucla.edu>
2022-11-29 22:37:57 -0800

    small change to createpost

--- v2/src/components/CreatePost.js
+++ v2/src/components/CreatePost.js
@@ -9,7 +9,6 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card'
 import CardHeader from '@mui/material/CardHeader'
 import CardContent from '@mui/material/CardContent'
-import CardMedia from '@mui/material/CardMedia'
 import CardActions from '@mui/material/CardActions'
 
 function CreatePost() {
@@ -50,7 +49,7 @@ function CreatePost() {
 
     return (
       <Card sx={{ width: 300, mt: 3, mr: 25}}>
-        <CardHeader title="Create a Post"></CardHeader>
+        <CardHeader title="Create a Post" />
         <CardContent>
             <TextField
                 sx={{ mt: 1, mb: 1}}
