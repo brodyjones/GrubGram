56f471571dfa3f7a48c9e0c090457a6bbd7c1ade
Sean Hoffmeister <seanhoffy@g.ucla.edu>
2022-11-29 19:47:00 -0800

    nav bar color scheme

--- v2/src/components/Navbar.js
+++ v2/src/components/Navbar.js
@@ -5,8 +5,23 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import FastfoodRoundedIcon from '@mui/icons-material/FastfoodRounded';
 import { auth, signOut } from "../firebase";
+import { red } from '@mui/material/colors';
+import { yellow } from '@mui/material/colors';
+
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: red[600],
+      },
+      secondary: {
+        main: yellow[500],
+      },
+    },
+});
 
 
 function Navbar() {
@@ -16,60 +31,66 @@ function Navbar() {
     };
 
     return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <FastfoodRoundedIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'white',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        GrubGram
-                    </Typography>
+        <ThemeProvider theme={theme}>
+            <AppBar color="primary" position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <FastfoodRoundedIcon color="secondary" sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                        <Typography
+                            variant="h6"
+                            color="secondary"
+                            noWrap
+                            component="a"
+                            sx={{
+                                mr: 2,
+                                display: { xs: 'none', md: 'flex' },
+                                fontFamily: 'monospace',
+                                fontWeight: 700,
+                                letterSpacing: '.3rem',
+                                textDecoration: 'none',
+                            }}
+                        >
+                            GrubGram
+                        </Typography>
 
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        <Button
-                            sx={{ my: 2, color: 'white' }}
-                            href="/home"
-                        >
-                            Home
-                        </Button>
-                        <Button
-                            sx={{ my: 2, color: 'white' }}
-                            href="/pantry"
-                        >
-                            Pantry
-                        </Button>
-                        <Button
-                            sx={{ my: 2, color: 'white' }}
-                            href="/recipes"
-                        >
-                            Recipes
-                        </Button>
-                    </Box>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                            <Button
+                                sx={{ my: 2 }}
+                                color="secondary"
+                                href="/home"
+                            >
+                                Home
+                            </Button>
+                            <Button
+                                sx={{ my: 2 }}
+                                color="secondary"
+                                href="/pantry"
+                            >
+                                Pantry
+                            </Button>
+                            <Button
+                                sx={{ my: 2 }}
+                                color="secondary"
+                                href="/recipes"
+                            >
+                                Recipes
+                            </Button>
+                        </Box>
 
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Button
-                            sx={{ my: 2, color: 'white' }}
-                            onClick={logout}
-                            href="/"
-                        >
-                            Logout
-                        </Button>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar >
+                        <Box sx={{ flexGrow: 0 }}>
+                            <Button
+                                sx={{ my: 2 }}
+                                color="secondary"
+                                onClick={logout}
+                                href="/"
+                            >
+                                Logout
+                            </Button>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar >
+        </ThemeProvider>
     );
 }
 export default Navbar;
