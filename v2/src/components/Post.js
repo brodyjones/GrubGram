2f2d913c7dc9a4dd833cfd71f41b96136db5a45b
Kavin Balakrishnan <kjbalakrishnan@ucla.edu>
2022-11-30 00:05:15 -0800

    Added PantryManager

--- v2/src/components/Post.js
+++ v2/src/components/Post.js
@@ -8,49 +8,49 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore'
-import {db} from '../firebase'
+import { db } from '../firebase'
 
 function Post() {
-    const [userPosts, setUserPosts] = useState([]);
-    const userPostsRef = collection(db, "user posts");
+  const [userPosts, setUserPosts] = useState([]);
+  const userPostsRef = collection(db, "user posts");
 
-    useEffect(() => {
-        const getUserPosts = async () => {
-          const data = await getDocs(userPostsRef);
-          setUserPosts(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
-        }
-    
-        getUserPosts()
-    }, [])
+  useEffect(() => {
+    const getUserPosts = async () => {
+      const data = await getDocs(userPostsRef);
+      setUserPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    }
 
-    return (
-      <div>
+    getUserPosts()
+  }, [])
+
+  return (
+    <div>
       {userPosts.map((userPost) => {
         return (
           <Card sx={{ maxWidth: 500, ml: 20, mt: 3 }}>
-          <CardHeader
-            title={userPost.username}
-            subheader={userPost.datecreated}
-          />
-          <CardMedia
-            component="img"
-            height="250"
-            image={userPost.imagelink}
-            alt="Paella dish"
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary"> 
-            {userPost.caption}
-            </Typography> 
-          </CardContent>
-          <CardActions>
-            <Button variant="contained">{userPost.recipe}</Button>
-          </CardActions>
-        </Card>
+            <CardHeader
+              title={userPost.username}
+              subheader={userPost.datecreated}
+            />
+            <CardMedia
+              component="img"
+              height="250"
+              image={userPost.imagelink}
+              alt="Paella dish"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {userPost.caption}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button variant="contained">{userPost.recipe}</Button>
+            </CardActions>
+          </Card>
         );
       })}
-      </div>
-    );
+    </div>
+  );
 }
 
 export default Post;
