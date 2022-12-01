fe41cd033e2d9771df1c75da337663b941aad879
Keith Hoffmeiser <keithhoffy7@g.ucla.edu>
2022-12-01 06:28:38 -0800

    working on user profile

--- v2/src/components/ProfileFeed.js
+++ v2/src/components/ProfileFeed.js
@@ -9,39 +9,44 @@ export default function ProfileFeed() {
     const [user] = useAuthState(auth);
 
     useEffect(() => {
+        if(user) return;
         const getUserPosts = async () => {
             const docRef = doc(db, "users", user?.uid);
             const docSnap = await getDoc(docRef);
-            setUserPosts(docSnap.data().posts);
+            const uPosts = docSnap.data().posts;
+            const ret = [];
+            uPosts.map(async (post) => {
+                const postsDocRef = doc(db, "posts", post);
+                const postsDocSnap = await getDoc(postsDocRef);
+                ret.push(postsDocSnap.data());
+            });
+            setUserPosts(ret);
         }
 
         getUserPosts();
     }, [user]);
-
+    
     return (
         <div>
-            {userPosts.map(async (post) => {
-                const postsDocRef = doc(db, "posts", post);
-                const postsDocSnap = await getDoc(postsDocRef);
+            {userPosts.map((post) => {
                 return (
                     <Card sx={{ maxWidth: 500, ml: 20, mt: 3 }}>
                         <CardHeader
-                            title={postsDocSnap.data().name}
-                            subheader={postsDocSnap.data().id}
+                            title={post.name}
+                            //subheader={post.id}
                         />
                         <CardMedia
                             component="img"
                             height="250"
-                            image={postsDocSnap.data().image}
-                            alt="Paella dish"
+                            image={post.image}
                         />
                         <CardContent>
                             <Typography variant="body2" color="text.secondary">
-                                {postsDocSnap.data().caption}
+                                {post.caption}
                             </Typography>
                         </CardContent>
                         <CardActions>
-                            <Button variant="contained">{postsDocSnap.data().recipe}</Button>
+                            <Button variant="contained">{post.recipe}</Button>
                         </CardActions>
                     </Card>
                 );
