09ce0ae0c553e86cfd306b87b85fc0dbc7f80237
Kriteen Jain <kriteenjain@gmail.com>
2022-12-01 20:40:36 -0800

    Update Profile Picture

--- v2/src/components/PofileCard.js
+++ v2/src/components/PofileCard.js
@@ -1,13 +1,16 @@
-import { Card, CardHeader, CardMedia } from "@mui/material";
+import { Button, Card, CardContent, CardHeader, CardMedia, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
-import { auth, db } from "../firebase";
-import { doc, getDoc } from 'firebase/firestore'
+import { auth, db, storage } from "../firebase";
+import { addDoc, doc, getDoc, setDoc, updateDoc } from 'firebase/firestore'
 import { useAuthState } from "react-firebase-hooks/auth";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export default function ProfileCard() {
     const [userInfo, setUserInfo] = useState([]);
     const [user] = useAuthState(auth);
-    
+    const [image, setImage] = useState(null);
+    const [imageLink, setImageLink] = useState("");
+
     useEffect(() => {
         const getUserInfo = async () => {
             const userRef = doc(db, "users", user?.uid);
@@ -18,15 +21,45 @@ export default function ProfileCard() {
         getUserInfo();
     }, [user]);
 
+    const UploadProfile = async () => {
+        if (!image) return;
+        const imageRef = ref(storage, `images/${image.name}`);
+        uploadBytes(imageRef, image);
+        getDownloadURL(ref(storage, `images/${image.name}`))
+            .then((url) => {
+                setImageLink(url);
+                updateUserLink(url);
+            })
+    }
+
+    const updateUserLink = async (ProfileLink) => {
+        const docRef = doc(db, "users", user?.uid);
+        await updateDoc(docRef, { profilePic: ProfileLink });
+        window.location.reload(false);
+    }
+
+
+
     return (
         <Card sx={{ maxWidth: 300, ml: 20, mt: 3 }}>
-            <CardHeader 
+            <CardHeader
                 title={userInfo.name}
             />
+            <CardContent>
+                <TextField type={"file"}
+                    sx={{ mt: 1, mb: 1, width: 267 }}
+                    id="filled-basic"
+                    onChange={(event) => {
+                        setImage(event.target.files[0]);
+                    }}
+                />
+                <Button variant="contained" onClick={UploadProfile}>Update Profile</Button>
+
+            </CardContent>
             <CardMedia
                 component="img"
                 height="250"
-                image="" //kriteen make this work as {userInfo.image}
+                image={userInfo.profilePic}
             />
         </Card>
     );
