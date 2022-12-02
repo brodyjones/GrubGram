569b3817aa74583c582824e5712ca744e4fda582
Keith Hoffmeiser <keithhoffy7@g.ucla.edu>
2022-12-02 06:05:41 -0800

    fixed bug in social page

--- v2/src/routes/Social.js
+++ v2/src/routes/Social.js
@@ -2,18 +2,24 @@ import { Avatar, Card, CardHeader, Grid } from "@mui/material";
 import { red } from "@mui/material/colors";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import PostFeed from "../components/PostFeed";
-import { db } from "../firebase";
+import { auth, db } from "../firebase";
 import './Profile.css'
 
 export default function Social() {
     const location = useLocation();
     const [userPosts, setUserPosts] = useState([]);
-    const [user, setUser] = useState([]);
+    const [userSoc, setUserSoc] = useState([]);
+    const [user, loading] = useAuthState(auth);
+    const navigate = useNavigate();
 
     useEffect(() => {
+        if (loading) return;
+        if (!user) navigate("/");
+
         const getUserPosts = async () => {
             const postsRef = collection(db, "posts");
             const q = query(postsRef, where("name", "==", location.state.post.name));
@@ -22,16 +28,16 @@ export default function Social() {
             setUserPosts(data);
         }
 
-        const getUser = async () => {
+        const getUserSoc = async () => {
             const usersRef = collection(db, "users");
             const q = query(usersRef, where("name", "==", location.state.post.name));
             const querySnapshot = await getDocs(q);
-            setUser(querySnapshot.docs[0].data());
+            setUserSoc(querySnapshot.docs[0].data());
         }
 
         getUserPosts();
-        getUser();
-    }, [])
+        getUserSoc();
+    }, [user, loading, navigate])
 
     return (
         <div>
@@ -46,10 +52,10 @@ export default function Social() {
                         direction="column"
                         alignItems="center"
                         justify="center">
-                        <CardHeader title={user.name} titleTypographyProps={{
+                        <CardHeader title={userSoc.name} titleTypographyProps={{
                             fontSize: 26, color: red[600], fontFamily: 'monospace', fontWeight: 'bold'
                         }} />
-                        <Avatar sx={{ mb: 3, height: 200, width: 200 }} src={user.profilePic} />
+                        <Avatar sx={{ mb: 3, height: 200, width: 200 }} src={userSoc.profilePic} />
                     </Grid>
                 </Card>
             </div>
